458d30d1e84d08fc6dc0ab639e5444f0de6ab656
Daniel Pawlik <dpawlik@redhat.com>
2023-11-10 19:02:12 +0100

    Update confige script; add deps for https dns proxy with quic
    Some deps should be installed when https-dns-proxy is installed [1].
    So far, https-dns-proxy is not supporting quic protocol, but you can
    always install package 'dnsproxy' [2] [3], that provides that.
    This commit is just installing deps packages to enable quic protocol
    on the main router when its available and improve the configure script.
    
    [1] https://docs.openwrt.melmac.net/https-dns-proxy/#http3-quic-support
    [2] https://forum.openwrt.org/t/dns-over-http-3/133092/24
    [3] https://github.com/AdguardTeam/dnsproxy?tab=readme-ov-file#usage

--- openwrt-configure.sh
+++ openwrt-configure.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# Example usage:
+# ./openwrt-configure.sh 192.168.88.1 main
+# ./openwrt-configure.sh 192.168.88.2
+
+ROUTER_IP="${ROUTER_IP=$1}"
 # DEVICE can be main or <nothing>
-DEVICE="${DEVICE:-$1}"
+DEVICE="${DEVICE:-$2}"
 FULL_WPAD="${FULL_WPAD:-'yes'}"
 INSTALL_BRIDGER=${INSTALL_BRIDGER:-'true'}
 CRYPTO_LIB=${CRYPTO_LIB:-'openssl'}
+
+if [ -z "$ROUTER_IP" ]; then
+    echo "Please provide router ip like: 192.168.1.1"
+    exit 1
+fi
 
 COMMAND="opkg update"
 #if [[ "$FULL_WPAD" =~ yes|Yes ]]; then
@@ -14,17 +24,16 @@ COMMAND="opkg update"
 # basic packages
 COMMAND="$COMMAND; opkg install collectd collectd-mod-sensors collectd-mod-irq \
 collectd-mod-dns collectd-mod-wireless \
-luci-app-statistics luci luci-i18n-base-pl vim htop \
+luci-app-statistics luci vim htop \
 curl iperf3 luci-app-attendedsysupgrade \
-auc bmon irqbalance"
-
+auc bmon irqbalance rsync"
 
 # additional packages
 if [[ "$DEVICE" =~ Main|main ]]; then
     COMMAND="$COMMAND collectd-mod-dhcpleases collectd-mod-thermal"
     COMMAND="$COMMAND luci-app-wireguard luci-proto-wireguard kmod-wireguard wireguard-tools qrencode"
-    COMMAND="$COMMAND https-dns-proxy luci-app-https-dns-proxy luci-i18n-https-dns-proxy-pl"
-    COMMAND="$COMMAND luci-app-sqm luci-i18n-sqm-pl collectd-mod-sqm sqm-scripts-extra"
+    COMMAND="$COMMAND https-dns-proxy luci-app-https-dns-proxy luci-i18n-https-dns-proxy-pl libcurl4 libnghttp3 libngtcp2"
+    COMMAND="$COMMAND luci-app-sqm collectd-mod-sqm"
     COMMAND="$COMMAND ddns-scripts luci-app-ddns bind-host"
 fi
 
@@ -34,16 +43,17 @@ fi
 
 COMMAND="$COMMAND; /etc/init.d/uhttpd start ; /etc/init.d/uhttpd enable;"
 
-read -n 1 -r -p "Should I execute command: $COMMAND ?" yn
+read -n 1 -r -p "Should I execute command: $COMMAND on root@$ROUTER_IP? " yn
 case $yn in
-    [Yy]* ) $COMMAND;;
+    [Yy]* ) ssh "root@$ROUTER_IP" "$COMMAND";;
     [Nn]* ) exit;;
     * ) echo "Please answer yes or no.";;
 esac
 
-read -n 1 -r -p "Should I reboot device?" yn
+echo -e "\n\nPackage installation completed!\n\n"
+read -n 1 -r -p "Should I reboot device $ROUTER_IP? " yn
 case $yn in
-    [Yy]* ) reboot;;
+    [Yy]* ) ssh "root@$ROUTER_IP" reboot;;
     [Nn]* ) exit;;
     * ) echo "Please answer yes or no.";;
 esac
@@ -52,13 +62,16 @@ esac
 # Add packages. NOTE: To install wpad-wolfssl, just replace the package name with wpad-basic-wolfssl
 ### basic
 #       opkg update;
-#       opkg install collectd collectd-mod-sensors collectd-mod-irq collectd-mod-dns collectd-mod-wireless luci-app-statistics luci luci-i18n-base-pl vim htop curl iperf3 luci-app-attendedsysupgrade auc bmon irqbalance
+#       opkg install collectd collectd-mod-sensors collectd-mod-irq collectd-mod-dns collectd-mod-wireless luci-app-statistics luci luci-i18n-base-pl vim htop curl iperf3 luci-app-attendedsysupgrade auc bmon irqbalance rsync
 
 ### wireguard
 #       luci-app-wireguard luci-proto-wireguard kmod-wireguard wireguard-tools qrencode
 
 ### DNS over HTTPS
-#       https-dns-proxy luci-app-https-dns-proxy luci-i18n-https-dns-proxy-pl
+#       https-dns-proxy luci-app-https-dns-proxy luci-i18n-https-dns-proxy-pl libcurl4 libnghttp3 libngtcp2
+
+### DDNS
+#       ddns-scripts luci-app-ddns bind-host
 
 ### Bufferbloat - install SQM - https://openwrt.org/docs/guide-user/network/traffic-shaping/sqm
 #       luci-app-sqm luci-i18n-sqm-pl collectd-mod-sqm sqm-scripts-extra
