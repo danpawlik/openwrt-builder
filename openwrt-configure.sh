d9ad056bdda8b63464f6982e83ba9f0004b2681f
Daniel Pawlik <dpawlik@redhat.com>
2024-01-08 11:47:20 +0100

    Disable DAWN

--- openwrt-configure.sh
+++ openwrt-configure.sh
@@ -9,6 +9,7 @@ ROUTER_IP="${ROUTER_IP=$1}"
 DEVICE="${DEVICE:-$2}"
 FULL_WPAD="${FULL_WPAD:-'yes'}"
 INSTALL_BRIDGER=${INSTALL_BRIDGER:-'true'}
+INSTALL_DAWN=${INSTALL_DAWN:-'false'}
 CRYPTO_LIB=${CRYPTO_LIB:-'openssl'}
 
 if [ -z "$ROUTER_IP" ]; then
@@ -26,7 +27,12 @@ COMMAND="$COMMAND; opkg install collectd collectd-mod-sensors \
 collectd-mod-dns collectd-mod-wireless \
 luci-app-statistics luci luci-i18n-base-pl vim htop \
 curl iperf3 luci-app-attendedsysupgrade \
-auc bmon irqbalance rsync dawn luci-app-dawn"
+auc bmon irqbalance luci-app-irqbalance rsync"
+
+if [[ "$INSTALL_DAWN" =~ True|true ]]; then
+    COMMAND="$COMMAND dawn luci-app-dawn"
+
+fi
 
 # additional packages
 if [[ "$DEVICE" =~ Main|main ]]; then
@@ -61,7 +67,7 @@ esac
 # Add packages. NOTE: To install wpad-wolfssl, just replace the package name with wpad-basic-wolfssl
 ### basic
 #       opkg update;
-#       opkg install collectd collectd-mod-sensors collectd-mod-dns collectd-mod-wireless luci-app-statistics luci luci-i18n-base-pl vim htop curl iperf3 luci-app-attendedsysupgrade auc bmon irqbalance rsync dawn luci-app-dawn
+#       opkg install collectd collectd-mod-sensors collectd-mod-dns collectd-mod-wireless luci-app-statistics luci luci-i18n-base-pl vim htop curl iperf3 luci-app-attendedsysupgrade auc bmon irqbalance luci-app-irqbalance rsync
 
 ### wireguard
 #       luci-app-wireguard luci-proto-wireguard kmod-wireguard wireguard-tools qrencode
